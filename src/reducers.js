be64b6475d198728dd3ae8f03e71b1bd4256deae
Jayanta <bjayanta.me@gmail.com>
2022-05-11 22:59:21 +0600

    setup done

---
+++ src/reducers.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "@reduxjs/toolkit";
+
+export const rootReducer = combineReducers({
+    
+});
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { rootReducer } from "./reducers";
+
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: [],
+    devTools: process.env.NODE_ENV === 'development'
+});
