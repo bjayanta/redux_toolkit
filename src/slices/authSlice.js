098d4e9c33548b6d45ae677628073b7f94613cae
Jayanta <bjayanta.me@gmail.com>
2022-05-12 08:16:53 +0600

    create slice

---
+++ src/slices/authSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState: {
+        isAuth: false,
+        phone: ""
+    },
+    reducers: {
+        setLogin: (state, action) => {
+            state.isAuth = true;
+            state.phone = action.payload
+        }
+    }
+});
